8624c55ee69fa0efd1207a31e27c501267c25809
Dxiaoer <lbm903803973@qq.com>
2022-05-11 16:39:52 +0800

    Initial commit

---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @date 2022/5/10 - 17:34
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ src/com/atguigu/java/HelloWorld2.java
@@ -0,0 +1,18 @@
+package com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @date 2022/5/10 - 17:35
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld22222!!!!!!!!");
+    }
+    public static final int NUM = 10;
+    /**
+    * 客户的姓名
+    */
+    private String name;
+
+
+}
